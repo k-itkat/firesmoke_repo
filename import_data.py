f7c6cdac69ae10f5a6c0930bb142eb21ed3caa6d
k-itkat <katrinapexa@gmail.com>
2025-06-16 12:03:43 -0700

    clean

--- import_data.py
+++ import_data.py
@@ -20,11 +20,17 @@ ax.set_title('Year-To-Date Smoke Cover by Province',fontsize=18)
 ax.set_ylabel('Smoke (km²)',fontsize=14)
 ax.set_xlabel('Province',fontsize=14)
 plt.text(.06, .98, f'Current as of {new_data_day}' '\n' 'Data Courtesy of ECCC/NOAA OSPO', ha='left', va='top', transform=ax.transAxes)
-plt.legend(bbox_to_anchor=(1, 1), loc="upper left")
+#plt.legend(bbox_to_anchor=(1, 0.8), loc="upper left",fontsize=14)
+plt.legend(loc='lower center',fontsize=14,bbox_to_anchor=(0.5,-0.05),ncol=6)
 
+box = ax.get_position()
+ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                 box.width, box.height * 0.9])
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
+          fancybox=True, ncol=6)
 ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
 
-fig2, ax2 = plt.subplots(1,1,figsize=(12,8))
-df_national.plot(kind='bar',ax=ax2,colormap='tab10')
-ax2.set_ylabel('Smoke (km²)',fontsize=14)
-ax2.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+# fig2, ax2 = plt.subplots(1,1,figsize=(12,8))
+# df_national.plot(kind='bar',ax=ax2,colormap='tab10')
+# ax2.set_ylabel('Smoke (km²)',fontsize=14)
+# ax2.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
